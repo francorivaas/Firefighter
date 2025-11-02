7a66174575cb0dc906c5aa4a473b31af529ffad9
francorivas-externo <francorivas-externo@tgacompany.com>
2025-11-02 09:49:32 -0300

    in dialogue system fixed

--- Assets/InGameDialogueTrigger.cs
+++ Assets/InGameDialogueTrigger.cs
@@ -5,7 +5,8 @@ public class InGameDialogueTrigger : MonoBehaviour
 {
     public InGameDialogue inGameDialogue;
     public Image sprite;
-    public string dialogueID = "D_1";
+    public string dialogueID;
+
     [TextArea(2, 5)] public string textoDelJefe;
 
     private bool triggered = false;
@@ -15,7 +16,7 @@ public class InGameDialogueTrigger : MonoBehaviour
         if (!triggered && collision.gameObject.CompareTag("Player")) 
         {
             triggered = false;
-            inGameDialogue.TriggerDialogue("D_1","No podrás rescatar a este ciudadano sin un cuchillo para cortar las cuerdas... seguro que hay uno por aquí. ¡Ve a buscarlo!", sprite);
+            inGameDialogue.TriggerDialogue(dialogueID, textoDelJefe, sprite);
         }
     }
 }
--- Assets/Scripts/VictoryTrigger.cs
+++ Assets/Scripts/VictoryTrigger.cs
@@ -3,7 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class VictoryTrigger : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
