72cecea02107bd5a0392ee35e12f9b5e1bbe9b95
francorivas-externo <francorivas-externo@tgacompany.com>
2025-11-05 11:55:55 -0300

    some project settings

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -35,6 +35,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        Cursor.visible = false;
         isCutscenePlayed = false;
         transition.SetActive(true);
     }
